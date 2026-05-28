2bfd60bdc25cdf04269e0e4bece06be7fa7a65e4
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-05-28 16:31:26 -0300

    feat: add Java configs

---
+++ server/src/test/java/com/elipse/observacao/ObservacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elipse.observacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
