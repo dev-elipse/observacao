e0bb90cc33f86a022575b53037c6c07cff0568bd
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-05-28 16:30:34 -0300

    feat: add ObservacaoApplication

---
+++ server/src/main/java/com/elipse/observacao/ObservacaoApplication.java
@@ -0,0 +1,13 @@
+package com.elipse.observacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservacaoApplication.class, args);
+	}
+
+}
